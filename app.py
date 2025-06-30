81a8263c22a977516b619505141e9a05ff477579
CodewithMukal <mukal.github@gmail.com>
2025-06-30 23:32:43 +0530

    upload

--- app.py
+++ app.py
@@ -75,5 +75,8 @@ def get_user(username):
     data = getData(username)
     return jsonify(data)
 
-if(__name__=="__main__"):
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
